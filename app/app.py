78ca2af5af801d88dcc987327e3d11dc2e5eb1b4
onigiri-jp1980 <d.yamashiro.mob@gmail.com>
2026-01-25 11:13:11 +0900

    マルチサービス化開始

--- app/app.py
+++ app/app.py
@@ -5,6 +5,19 @@ from typing import List, Dict
 from pprint import pprint
 from argparse import ArgumentParser,RawTextHelpFormatter
 
+headers={
+    'ec2':[
+        'インスタンス名','インスタンスID','インスタンスタイプ','ステータス','起動時間'
+    ],
+    'rds':[
+        'DBインスタンス名','DBインスタンスID','DBインスタンスタイプ','ステータス','起動時間'
+    ],
+    'sewcurity_group':[
+        'セキュリティグループ名','セキュリティグループID','VPCID','Description'
+    ]
+}
+
+
 def parse_args():
     parser=ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument('--profile', type=str, default='default', help='AWS profile name')
@@ -15,15 +28,16 @@ def parse_args():
 
 def get_ec2_client(
     profile_name: str='default',
-    region_name: str='us-east-1')->boto3.client.EC2:
+    region_name: str='us-east-1',
+    service_name: str='ec2')->boto3.client.EC2:
     session = Session(profile_name=profile_name)
-    return session.client('ec2', region_name=region_name)
+    return session.client(service_name, region_name=region_name)
 
 def get_ec2_instances(client: boto3.client.EC2) -> List[Dict]:
     response = client.describe_instances()
     return [i for r in response['Reservations'] for i in r['Instances']]
 
-def build_ec2_instances_csv(data: List[Dict]) -> str:
+def build_csv(data: List[Dict],service_name: str) -> str:
     csv_data=[]
     for instance in data:
         instance_name=next((t['Value'] for t in instance['Tags'] if t['Key'] == 'Name'), '')
@@ -38,21 +52,10 @@ def build_ec2_instances_csv(data: List[Dict]) -> str:
 
 def main():
     args=parse_args()
-    hearders=[
-        'インスタンス名','インスタンスID','インスタンスタイプ','ステータス','起動時間'
-    ]
-    ec2=get_ec2_client(profile_name=args.profile,region_name=args.region)
-    if not path.exists('ec2_instances.json'):
-        print('ec2_instances.json not found')
-        data=build_ec2_instances_csv(get_ec2_instances(ec2))
-    else:
-        print('ec2_instances.json exists.')
-        with open('ec2_instances.json', 'r') as f:
-            data=json.load(f)
-    with open('ec2_instances.csv', 'w') as f:
-        writer=csv.writer(f)
-        writer.writerow(hearders)
-        writer.writerows(build_ec2_instances_csv(data),)
+    service=args.service if args.service != 'security_group' else 'vpc'
+    client=get_client(profile_name=args.profile,region_name=args.region,service_name=service)
+    data=get_data(client)
+    build_csv(data,service)
     return
 
 if __name__ == '__main__':
